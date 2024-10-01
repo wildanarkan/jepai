91426429c7e442ce6db829535c547a1b2a8f6dbb
wildanarkan <cillmystic@gmail.com>
2024-10-01 20:18:27 +0700

    chore: Configure Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:jepai/firebase_options.dart';
 import 'package:jepai/views/home_page.dart';
 import 'package:jepai/views/introduction_page.dart';
 import 'package:jepai/views/login_page.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   await GetStorage.init();
   runApp(const JepaiApp());
 }
