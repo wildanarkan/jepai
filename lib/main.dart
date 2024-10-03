98b626551b06bc2ea7658f3824a45f694e9e4204
wildanarkan <cillmystic@gmail.com>
2024-10-04 05:36:18 +0700

    chore: Configure Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
   await GetStorage.init();
   runApp(const JepaiApp());
 }
